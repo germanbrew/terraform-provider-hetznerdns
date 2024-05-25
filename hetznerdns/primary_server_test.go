b661725cd37c7242d27780729bf945dda6bbdf5d
Kim Oliver Drechsel <kim@drechsel.xyz>
2024-05-26 00:25:50 +0200

    Fix test TestAccTwoPrimaryServersResources
    Signed-off-by: Kim Oliver Drechsel <kim@drechsel.xyz>

--- hetznerdns/primary_server_test.go
+++ hetznerdns/primary_server_test.go
@@ -57,10 +57,10 @@ func TestAccTwoPrimaryServersResources(t *testing.T) {
 	aZoneName := fmt.Sprintf("%s.online", acctest.RandString(10))
 	aZoneTTL := 60
 
-	ps1Address := "1.1.1.1"
+	ps1Address := "1.1.0.0"
 	ps1Port := 53
 
-	ps2Address := "1.1.0.0"
+	ps2Address := "2.2.0.0"
 	ps2Port := 53
 
 	resource.Test(t, resource.TestCase{
