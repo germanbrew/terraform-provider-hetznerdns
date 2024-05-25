38b14ceb02cedda791ca7ed84dec1a702a46b0c9
Kim Oliver Drechsel <kim@drechsel.xyz>
2024-05-26 00:55:35 +0200

    Adjust ip address
    Signed-off-by: Kim Oliver Drechsel <kim@drechsel.xyz>

--- hetznerdns/primary_server_test.go
+++ hetznerdns/primary_server_test.go
@@ -14,7 +14,7 @@ func TestAccPrimaryServerResources(t *testing.T) {
 	aZoneName := fmt.Sprintf("%s.online", acctest.RandString(10))
 	aZoneTTL := 60
 
-	psAddress := "1.1.1.1"
+	psAddress := "1.1.0.0"
 	psPort := 53
 
 	resource.Test(t, resource.TestCase{
