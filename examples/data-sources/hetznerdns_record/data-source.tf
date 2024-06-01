a7f72be8ea900b6661d6c047beafe2ce1ecc4df0
Kim Oliver Drechsel <kim@drechsel.xyz>
2024-06-01 09:02:49 +0200

    Adjust example
    Signed-off-by: Kim Oliver Drechsel <kim@drechsel.xyz>

--- examples/data-sources/hetznerdns_record/data-source.tf
+++ examples/data-sources/hetznerdns_record/data-source.tf
@@ -2,6 +2,10 @@ data "hetznerdns_zone" "zone1" {
   name = "zone1.online"
 }
 
-data "hetznerdns_record" "record1" {
+data "hetznerdns_records" "zone1" {
   zone_id = data.hetznerdns_zone.zone1.id
+}
+
+output "zone1_records" {
+  value = data.hetznerdns_records.zone1.records
 }
