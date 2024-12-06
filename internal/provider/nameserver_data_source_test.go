9865c62249a2e27987cd199b68b71de8113b98b0
Kim Oliver Drechsel <kim@drechsel.xyz>
2024-12-06 16:47:10 +0100

    test: add additional tests for nameserver data source
    Signed-off-by: Kim Oliver Drechsel <kim@drechsel.xyz>

--- internal/provider/nameserver_data_source_test.go
+++ internal/provider/nameserver_data_source_test.go
@@ -2,6 +2,7 @@ package provider
 
 import (
 	"context"
+	"sort"
 	"strings"
 	"testing"
 
@@ -10,6 +11,19 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
 	"github.com/hashicorp/terraform-plugin-testing/statecheck"
 )
+
+// prepareKnownValues prepares a list of known values for the given nameserver list.
+func prepareKnownValues(nameserver []string) []knownvalue.Check {
+	knownValues := make([]knownvalue.Check, len(nameserver))
+	// Sort the list to ensure the order is consistent and matches the output of the data source.
+	sort.Strings(nameserver)
+
+	for i, ip := range nameserver {
+		knownValues[i] = knownvalue.StringExact(ip)
+	}
+
+	return knownValues
+}
 
 func TestAccNameservers_DataSource(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
@@ -25,6 +39,25 @@ func TestAccNameservers_DataSource(t *testing.T) {
 	for i, ns := range authorizedNameservers {
 		nsNames[i] = knownvalue.StringExact(ns["name"])
 	}
+
+	// The IPv4 addresses of the authoritative nameservers to check against.
+	// https://docs.hetzner.com/dns-console/dns/general/authoritative-name-servers/#new-name-servers-for-robot-and-cloud-console-customers
+	authorizedNsIPv4 := []string{
+		"193.47.99.5",
+		"213.133.100.98",
+		"88.198.229.192",
+	}
+
+	// The IPv6 addresses of the secondary nameservers to check against.
+	// https://docs.hetzner.com/dns-console/dns/general/authoritative-name-servers/#secondary-dns-servers-old-name-servers-for-robot-customers
+	secondaryNsIPv6 := []string{
+		"2a01:4f8:0:a101::a:1",
+		"2a01:4f8:0:1::5ddc:2",
+		"2001:67c:192c::add:a3",
+	}
+
+	authorizedIPv4 := prepareKnownValues(authorizedNsIPv4)
+	secondaryIPv6 := prepareKnownValues(secondaryNsIPv6)
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
@@ -43,6 +76,8 @@ func TestAccNameservers_DataSource(t *testing.T) {
 				),
 				ConfigStateChecks: []statecheck.StateCheck{
 					statecheck.ExpectKnownOutputValue("primary_names", knownvalue.ListExact(nsNames)),
+					statecheck.ExpectKnownOutputValue("primary_ipv4s", knownvalue.ListExact(authorizedIPv4)),
+					statecheck.ExpectKnownOutputValue("secondary_ipv6s", knownvalue.ListExact(secondaryIPv6)),
 				},
 			},
 		},
@@ -65,6 +100,14 @@ data "hetznerdns_nameservers" "konsoleh" {
 
 output "primary_names" {
 	value = data.hetznerdns_nameservers.primary.ns.*.name
+}
+
+output "primary_ipv4s" {
+	value = data.hetznerdns_nameservers.primary.ns.*.ipv4
+}
+
+output "secondary_ipv6s" {
+	value = data.hetznerdns_nameservers.secondary.ns.*.ipv6
 }
 `
 }
