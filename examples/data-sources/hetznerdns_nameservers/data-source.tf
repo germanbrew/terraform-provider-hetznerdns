0492fd62fe8167d4dcd0fe8982e401282c04c17d
Kim Oliver Drechsel <kim@drechsel.xyz>
2024-12-06 17:24:11 +0100

    docs: fix nameserver data source example
    Signed-off-by: Kim Oliver Drechsel <kim@drechsel.xyz>

--- examples/data-sources/hetznerdns_nameservers/data-source.tf
+++ examples/data-sources/hetznerdns_nameservers/data-source.tf
@@ -1,9 +1,9 @@
-data "hetznerdns_nameserver" "primary" {
+data "hetznerdns_nameservers" "primary" {
   type = "authoritative"
 }
 
 resource "hetznerdns_record" "mydomain_de-NS" {
-  for_each = toset(data.hetznerdns_nameserver.primary.ns.*.name)
+  for_each = toset(data.hetznerdns_nameservers.primary.ns.*.name)
 
   zone_id = hetznerdns_zone.de.id
   name    = "@"
